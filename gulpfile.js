196edd5e51f82a56a24b532d32f8aa0eac3ae9ab
Merrick Christensen <merrick.christensen@gmail.com>
2015-01-01 16:42:29 -0700

    add active

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var data = require('gulp-data');
+var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   gulp.src('./src/*.jade')
-    .pipe(jade({
+    .pipe(data(function(file) {
+      return {
+        path: path.basename(file.path)
+      }
     }))
+    .pipe(jade({}))
     .pipe(gulp.dest('./compiled/'));
 
   gulp.src('./src/public/**/*')
